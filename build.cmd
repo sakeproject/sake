a8815b68f84a8896015722a3aa2ef6ff7cfb2f84
Louis DeJardin <louis.dejardin@gmail.com>
2012-05-02 16:01:56 -0700

    Adding to common library
    xunit, directory, and assemblyinfo tags
    Regex import

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 :@echo off
 set EnableNuGetPackageRestore=true
-"%~dp0.nuget\NuGet.exe" install -OutputDirectory packages .\packages.config
-"%~dp0packages\Sake.0.1.0-alpha\tools\Sake.exe" -C %~dp0 -I src\Sake.Library\Shared %*
+".nuget\NuGet.exe" install .\packages.config -o packages
+"packages\Sake.0.1.0-alpha\tools\Sake.exe" -C %~dp0 -I src\Sake.Library\Shared %*
